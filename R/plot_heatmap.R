2c3d178f254dec7ead9c907130647f3194b91510
Ethan Baldwin <eab77806@uga.edu>
2025-06-17 22:44:00 -0400

    add helper function for testing

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -47,3 +47,34 @@ plot_quaint_heatmap <- function(quaint_results,tree,outgroup,plot_val,cell_text=
   
   heatmap
 }
+
+get_plot_df <- function(quaint_results,tree,outgroup,plot_val,cell_text=FALSE) {
+  tip_labels<-setdiff(tree$tip.label,outgroup)
+  combos<-expand.grid(tip_labels,tip_labels)
+  
+  quaint_results_reversed<-quaint_results%>%
+    rename(Var1=taxon2,Var2=taxon1)
+  
+  quaint_results<-quaint_results%>%
+    rename(Var1=taxon1,Var2=taxon2)
+  
+  all_quaint_results<-bind_rows(quaint_results,quaint_results_reversed)
+  
+  combos_with_position<-combos%>%
+    left_join(all_quaint_results,by=c("Var1","Var2"))
+  
+  # get order of tip labels
+  edge_order <- tree$edge[,2]
+  n_tips_vector <- 1:length(tree$tip.label)
+  vector_order <- edge_order[edge_order %in% n_tips_vector]
+  tip_order <- rev(tree$tip.label[vector_order])
+  
+  # set order of axes
+  combos_with_position$tax1<-factor(combos_with_position$Var1,levels=rev(tip_order))
+  combos_with_position$tax2<-factor(combos_with_position$Var2,levels=tip_order)
+  
+  combos_with_position$stat <- combos_with_position[[plot_val]]
+  combos_with_position$stat_rounded<-round(combos_with_position$stat,2)
+  
+  return(combos_with_position)
+}
