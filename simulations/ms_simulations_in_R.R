80c97b18c6f5f955fb7586bc5f60e0cd88ca46fc
Ethan Baldwin <eab77806@uga.edu>
2025-09-24 09:38:37 -0400

    Update ms_simulations_in_R.R

--- simulations/ms_simulations_in_R.R
+++ simulations/ms_simulations_in_R.R
@@ -56,7 +56,7 @@ simulate_gene_trees <- function(species_tree, num_trees = 100, migration_matrix 
   
   # Construct ms command
   ms_opts <- paste("-T -I", num_tips, paste(rep(1, num_tips), collapse=" "), paste(ms_commands, collapse=" "), migration_command)
-
+  print(ms_opts)
   ms_out <- ms(nsam=num_tips, nreps=num_trees, opts=ms_opts)
   trees <- get_trees_from_ms(ms_out)
 
@@ -69,12 +69,78 @@ simulate_gene_trees <- function(species_tree, num_trees = 100, migration_matrix 
   return(trees)
 }
 
-
+simulate_gene_trees_es <- function(species_tree, num_trees = 100, hybridization_events = NULL) {
+  species_tree$node.label <- NULL
+  if(any(branching.times(species_tree)<0)) {
+    species_tree <- make_ultrametric(species_tree)
+  }
+  
+  branching_times <- branching.times(species_tree)
+  sorted_events <- sort(branching_times)
+  
+  num_tips <- length(species_tree$tip.label)
+  ms_commands <- character()
+  
+  node_lookup <- c()
+  
+  for (i in seq_along(sorted_events)) {
+    node <- as.numeric(names(sorted_events)[i])
+    time <- sorted_events[i]
+    
+    descendants <- species_tree$edge[species_tree$edge[, 1] == node, 2]
+    
+    desc1 <- ifelse(descendants[1] <= num_tips, descendants[1], node_lookup[descendants[1]])
+    desc2 <- ifelse(descendants[2] <= num_tips, descendants[2], node_lookup[descendants[2]])
+    
+    ms_commands <- c(ms_commands, sprintf("-ej %f %d %d", time, desc1, desc2))
+    node_lookup[node] <- desc2
+  }
+  
+  # Add migration settings if provided
+  # migration_command <- ""
+  # if (!is.null(hybridization_matrix)) {
+  #   migration_command <- paste0("-es ",paste(apply(migration_matrix, 1, paste, collapse=" "), collapse=" -em "))
+  # }
+  
+  hybrid_commands <- character()
+  if (!is.null(hybridization_events)) {
+    new_pop_id <- num_tips
+    for (i in seq_len(nrow(hybridization_events))) {
+      ev <- hybridization_events[i, ]
+      if(i==1){
+        hybrid_commands <- sprintf("-es %f %d %f", ev$time, ev$recipient, ev$frac)
+      }
+      else{
+        hybrid_commands <- paste(hybrid_commands,
+                                 sprintf("-es %f %d %f", ev$time, ev$recipient, ev$frac))
+      }
+      
+      new_pop_id <- new_pop_id + 1
+      # Merge donor with the new pop (same time)
+      hybrid_commands <- paste(hybrid_commands,
+                           sprintf("-ej %f %d %d", ev$time, new_pop_id, ev$donor))
+    }
+  }
+  
+  # Construct ms command
+  ms_opts <- paste("-T -I", num_tips, paste(rep(1, num_tips), collapse=" "), paste(ms_commands, collapse=" "), hybrid_commands)
+  print(ms_opts)
+  ms_out <- ms(nsam=num_tips, nreps=num_trees, opts=ms_opts)
+  trees <- get_trees_from_ms(ms_out)
+  
+  # Restore original tip labels
+  for (i in seq_along(trees)) {
+    tip_labels <- trees[[i]]$tip.label
+    trees[[i]]$tip.label <- species_tree$tip.label[as.integer(gsub("s([0-9]+)", "\\1", tip_labels))]
+  }
+  
+  return(trees)
+}
 # Example usage
 # species_tree_newick <- "(A:1, ((B:1,C:1):1, (D:1,E:1):1):1);"
 # species_tree1 <- read.tree(text = species_tree_newick)
 # 
-# gene_trees1 <- simulate_gene_trees(species_tree1)
+  # gene_trees1 <- simulate_gene_trees(species_tree1)
 # 
 # species_tree_2 <- read.tree("~/Research/genome_assembly/manuscript/trees/astral4_BS10.tre")
 # 
